5a5a480802d2ddbe3426b1d7d002619e7000bfaa
Nickolas Luciano <nickolas333@hotmail.com>
2023-06-15 16:36:47 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Foto de Nickolas Luciano modo claro")
+  } else {
+    //se tiver sem light mode, manter imagem normal
+  }
+}
